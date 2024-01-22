cf6f7b2a2478eb7df4dc2f6af40843a1f43e06e9
Mahmudtareq <tareqlpu5@gmail.com>
2024-01-22 10:41:53 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-3xl font-bold underline">
+        Vite + React + Taliwind css +js
+      </h1>
+      <h1 className="text-3xl font-bold underline">Template Project </h1>
+    </div>
+  );
+};
+
+export default App;
